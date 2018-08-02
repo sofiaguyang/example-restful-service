e8b466a0ac3c9dad38037725d0f65cd01199afdc
Sofia Ang <sofia.indira.odessa.ang@asia.ing.com>
2018-08-02 15:38:31 +0800

    Shorten package name

--- src/main/groovy/com/github/sofiaguyang/example/Application.groovy
+++ src/main/groovy/com/github/sofiaguyang/example/Application.groovy
@@ -1,4 +1,4 @@
-package com.github.sofiaguyang.examplewebservice
+package com.github.sofiaguyang.example
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
