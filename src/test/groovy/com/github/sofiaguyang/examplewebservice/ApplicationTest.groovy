c2bd29216a648a7141cc7e8526b778cc50fa470f
Sofia <sofiaguyang@users.noreply.github.com>
2018-08-02 10:39:12 +0800

    Initialize project

---
+++ src/test/groovy/com/github/sofiaguyang/examplewebservice/ApplicationTest.groovy
@@ -0,0 +1,15 @@
+package com.github.sofiaguyang.examplewebservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
